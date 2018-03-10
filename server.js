50f106db77bf54b956dcf022c3bbdc3fbe96140e
JOHNFLEURIMOND <johnnyxxboston@gmail.com>
2018-03-09 21:32:14 -0500

    it works

--- server.js
+++ server.js
@@ -14,42 +14,79 @@ MongoClient.connect('mongodb://demo:demo@ds125146.mlab.com:25146/savage', (err, 
 })
 
 app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   db.collection('messages').find().toArray((err, result) => {
     if (err) return console.log(err)
-    res.render('index.ejs', {messages: result})
+    res.render('index.ejs', {
+      messages: result
+    })
   })
 })
 
 app.post('/messages', (req, res) => {
-  db.collection('messages').save({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+  db.collection('messages').save({
+    name: req.body.name,
+    msg: req.body.msg,
+    thumbUp: 0,
+    thumbDown: 0
+  }, (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
   })
 })
 
-app.put('/messages', (req, res) => {
+app.put('/thumbUp', (req, res) => {
   db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
-    $set: {
-      thumbUp:req.body.thumbUp + 1
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
+    .findOneAndUpdate({
+      name: req.body.name,
+      msg: req.body.msg
+    }, {
+      $set: {
+        thumbUp: req.body.thumbUp + 1
+      }
+    }, {
+      sort: {
+        _id: -1
+      },
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    })
+})
+
+app.put('/thumbDown', (req, res) => {
+  db.collection('messages')
+    .findOneAndUpdate({
+      name: req.body.name,
+      msg: req.body.msg
+    }, {
+      $set: {
+        thumbDown: req.body.thumbDown + 1
+      }
+    }, {
+      sort: {
+        _id: -1
+      },
+      upsert: true
+    }, (err, result) => {
+      if (err) return res.send(err)
+      res.send(result)
+    })
 })
 
 app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+  db.collection('messages').findOneAndDelete({
+    name: req.body.name,
+    msg: req.body.msg
+  }, (err, result) => {
     if (err) return res.send(500, err)
     res.send('Message deleted!')
   })
